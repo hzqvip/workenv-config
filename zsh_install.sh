ee9a11ed4815409c9d5888d4c203e591693253a0
hzqvip <33923663+hzqvip@users.noreply.github.com>
2018-07-09 19:12:00 +0800

    Update zsh_install.sh

--- zsh_install.sh
+++ zsh_install.sh
@@ -5,7 +5,7 @@ sudo spctl --master-disable
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-ln -s $DIR/.vimrc ~/
+cp $DIR/.vimrc ~/
 
 # add two source persion post_zshrc
 sed -i 's#source $ZSH/oh-my-zsh.sh#source '$DIR'/.pre_zshrc \
